55dd7238255bfdfc15161aa337c4f9c66f0d989a
John <john@hackade.com>
2023-04-11 17:49:39 -0500

    Added comments to test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+
+# Reset the blockchain
 ./resetChain.sh
+
+# Find all files in the ./content directory and run ./addblock.sh on each file
 find ./content -type f | xargs -n 1 ./addblock.sh
+
+# Verify the blocks using Python script
 python3 ./verify_blocks.py
